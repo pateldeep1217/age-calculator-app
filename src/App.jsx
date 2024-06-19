59d75e85afe070439d08aa6effbe5b980253a98b
Deep Patel <patel_deep1217@outlook.com>
2024-06-19 12:26:39 -0400

    Add button arrow icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import CustomInput from "./components/CustomInput"
+import arrowIcon from './assets/icon-arrow.svg'
+import ButtonArrow from "./components/ButtonArrow"
+
 
 
 
@@ -7,25 +10,30 @@ function App() {
 
 
   return (
-    <main className="font-poppins h-screen w-screen bg-neutral-light-grey flex flex-col">
+    <main className="font-poppins h-screen w-screen bg-neutral-light-grey border">
       
-      <section className=" bg-white mt-20 flex justify-between p-5 w-[343px] lg:w-[840px] mx-auto">
+      <div className="bg-white mt-20  p-5 w-[343px] lg:w-[840px] mx-auto rounded-3xl rounded-br-[100px]">
 
-        <div className="  flex flex-col">
-          <label htmlFor="day" className="font-400i text-neutral-smokey-grey text-xs uppercase tracking-widest pb-1">Day</label>
-          <CustomInput/>
-        </div>
+          <div className=" flex justify-between">
+            <div className="  flex flex-col">
+              <label htmlFor="day" className="font-400i text-neutral-smokey-grey text-xs uppercase tracking-widest pb-1">Day</label>
+              <CustomInput/>
+            </div>
 
-        <div className="   flex flex-col">
-          <label htmlFor="day" className="font-400i text-neutral-smokey-grey text-xs uppercase tracking-widest pb-1">Month</label>
-          <CustomInput/>
-        </div>
+            <div className="   flex flex-col">
+              <label htmlFor="day" className="font-400i text-neutral-smokey-grey text-xs uppercase tracking-widest pb-1">Month</label>
+              <CustomInput/>
+            </div>
 
-        <div className="  flex flex-col ">
-          <label htmlFor="day" className="font-400i text-neutral-smokey-grey text-xs uppercase tracking-widest pb-1">Year</label>
-          <CustomInput/>
-        </div>
-      </section>
+            <div className="  flex flex-col ">
+              <label htmlFor="day" className="font-400i text-neutral-smokey-grey text-xs uppercase tracking-widest pb-1">Year</label>
+              <CustomInput/>
+            </div>
+          </div>
+        <ButtonArrow/>
+          
+
+      </div>
 
       
     </main>
---
+++ src/components/ButtonArrow.jsx
@@ -0,0 +1,13 @@
+
+
+function ButtonArrow() {
+  return (
+    <button className="bg-primary-purple w-[64px] h-[64px]  flex justify-center items-center rounded-full">
+    <span>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" strokeWidth="4"><path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>
+    </span>
+  </button>
+  )
+}
+
+export default ButtonArrow
