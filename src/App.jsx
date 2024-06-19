aa36b8194ff506c64e0756fc9c339c588001d2f8
Deep Patel <patel_deep1217@outlook.com>
2024-06-19 09:46:52 -0400

    add form labels and inputs to the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import CustomInput from "./components/CustomInput"
+
 
 
 
@@ -5,8 +7,27 @@ function App() {
 
 
   return (
-    <main>
-      <h1>Age Calculator</h1>
+    <main className="font-poppins h-screen w-screen bg-neutral-light-grey flex flex-col">
+      
+      <section className=" bg-white mt-20 flex justify-between p-5 w-[343px] lg:w-[840px] mx-auto">
+
+        <div className="  flex flex-col">
+          <label htmlFor="day" className="font-400i text-neutral-smokey-grey text-xs uppercase tracking-widest pb-1">Day</label>
+          <CustomInput/>
+        </div>
+
+        <div className="   flex flex-col">
+          <label htmlFor="day" className="font-400i text-neutral-smokey-grey text-xs uppercase tracking-widest pb-1">Month</label>
+          <CustomInput/>
+        </div>
+
+        <div className="  flex flex-col ">
+          <label htmlFor="day" className="font-400i text-neutral-smokey-grey text-xs uppercase tracking-widest pb-1">Year</label>
+          <CustomInput/>
+        </div>
+      </section>
+
+      
     </main>
   )
 }
