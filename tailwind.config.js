98fe7975f85db445dd0fc57f28b443b177e7423e
Deep Patel <patel_deep1217@outlook.com>
2024-06-19 09:38:02 -0400

    update tailwind config settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,39 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '375px',   
+      'lg': '1440px',  
+    },
+    extend: {
+      letterSpacing:{
+        widest: '.25rem',
+      },
+      fontSize: {
+        '32px': '32px',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      }, 
+      fontWeight: {
+        '400i': '400',
+        '700': '700',
+        '800i': '800',
+      },
+      colors:{
+        primary:{
+          purple:'hsl(259, 100%, 65%)',
+          'light-red': 'hsl(0, 100%, 67%)'
+        },
+        neutral:{
+          white: 'hsl(0, 0%, 100%)',
+          'off-white': 'hsl(0, 0%, 94%)',
+          'light-grey': 'hsl(0, 0%, 86%)',
+          'smokey-grey': 'hsl(0, 1%, 44%)',
+          'off-black': 'hsl(0, 0%, 8%)',
+        }
+      }
+    },
   },
   plugins: [],
 }
